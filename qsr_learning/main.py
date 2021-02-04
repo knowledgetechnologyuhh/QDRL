248bcf1756908aa052cd0eb33391b07e399a699b
Jae Hee Lee <dschaehi@gmail.com>
2021-02-04 22:45:32 +0100

    using ray tune

--- qsr_learning/main.py
+++ qsr_learning/main.py
@@ -1,14 +1,22 @@
+import argparse
 import os
+from functools import partial
+from pathlib import Path
 
+import git
 import torch
+from git.exc import RepositoryDirtyError
 from munch import Munch
+from ray import tune
 
 from qsr_learning.relations.absolute.half_planes import left_of, right_of
 from qsr_learning.train import train
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3,4,5,6,7"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+repo = git.Repo(Path(".").absolute(), search_parent_directories=True)
+sha = repo.head.object.hexsha
+ROOT = Path(repo.working_tree_dir)
 
 config = Munch(
     model=Munch(
@@ -44,4 +52,34 @@ config = Munch(
     ),
 )
 
-train(config, device)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--smoke-test", action="store_true", help="Finish quickly for testing"
+    )
+    parser.add_argument(
+        "--debug", action="store_true", help="Debugging. Repo can be dirty."
+    )
+    args, _ = parser.parse_known_args()
+
+    if not args.debug and repo.is_dirty():
+        raise RepositoryDirtyError(repo, "Have you forgotten to commit the changes?")
+
+    current_path = Path(repo.working_dir).as_posix()
+    repo_name = repo.remotes.origin.url.split(".git")[0].split("/")[-1]
+    if args.debug:
+        repo_name = "[dirty]" + repo_name
+    experiment_name = repo_name + "_" + sha[:8]
+
+    analysis = tune.run(
+        partial(train, device=device),
+        metric="validation_accuracy",
+        mode="max",
+        name=experiment_name,
+        stop={"training_iteration": 1} if args.smoke_test else None,
+        config=config,
+        resources_per_trial={"cpu": 6, "gpu": 1},
+        num_samples=1 if args.smoke_test else 64,
+        keep_checkpoints_num=1,
+        checkpoint_score_attr="validation_accuracy",
+    )
--- qsr_learning/train.py
+++ qsr_learning/train.py
@@ -1,23 +1,26 @@
 import torch
 import torch.nn as nn
 from munch import Munch
+from ray import tune
 from torch.utils.data import DataLoader
-from tqdm.auto import trange
 
 from qsr_learning.data.data import QSRData
 from qsr_learning.models.simple_baseline import Net
 
 
-def format_result(phase, result, data_loader):
-    result_dict = {
-        "phase": phase[:5],
-        "loss": result.total_loss[phase] / len(data_loader[phase].dataset),
-        "accuracy": result.num_correct[phase] / len(data_loader[phase].dataset),
-    }
-    return "[{phase}] loss: {loss:4.3f}, acc: {accuracy:4.3f}".format(**result_dict)
+def report_result(epoch, phases, result, data_loader):
+    log = dict(epoch=epoch)
+    for phase in phases:
+        log[phase + "_loss"] = result[phase].total_loss / len(
+            data_loader[phase].dataset
+        )
+        log[phase + "_accuracy"] = result[phase].num_correct / len(
+            data_loader[phase].dataset
+        )
+    tune.report(**log)
 
 
-def train_step(model, criterion, optimizer, phase, batch, result, device):
+def step(model, criterion, optimizer, phase, batch, result, device):
     model.train() if phase == "train" else model.eval()
     torch.autograd.set_grad_enabled(phase == "train")
     images, questions, answers = (item.to(device) for item in batch)
@@ -25,19 +28,19 @@ def train_step(model, criterion, optimizer, phase, batch, result, device):
     model.zero_grad()
     out = model(images, questions)
     loss = criterion(out, answers.float()) / batch_size
-    result.total_loss[phase] += loss.item()
-    result.num_correct[phase] += ((out > 0.5) == answers).sum().item()
+    result[phase].total_loss += loss.item()
+    result[phase].num_correct += ((out > 0.5) == answers).sum().item()
     if phase == "train":
         loss.backward()
         optimizer.step()
 
 
-def train_epoch(phase, epoch, result, data_loader, model, criterion, optimizer, device):
-    result.total_loss[phase] = 0
-    result.num_correct[phase] = 0
-    for batch in data_loader[phase]:
-        train_step(model, criterion, optimizer, phase, batch, result, device)
-    print(epoch, format_result(phase, result, data_loader))
+# def train_epoch(phase, epoch, result, data_loader, model, criterion, optimizer, device):
+#     result.total_loss[phase] = 0
+#     result.num_correct[phase] = 0
+#     for batch in data_loader[phase]:
+#         train_step(model, criterion, optimizer, phase, batch, result, device)
+#     print(epoch, format_result(phase, result, data_loader))
 
 
 def train(config, device):
@@ -59,20 +62,15 @@ def train(config, device):
     model.to(device)
     criterion = nn.BCELoss(reduction="sum")
     optimizer = torch.optim.Adam(model.parameters())
-    with trange(config.train.num_epochs) as pbar:
-        for epoch in pbar:
-            result = Munch(
-                total_loss=Munch(),
-                num_correct=Munch(),
-            )
-            for phase in phases:
-                result.total_loss[phase] = 0
-                result.num_correct[phase] = 0
-                for batch in data_loader[phase]:
-                    train_step(
-                        model, criterion, optimizer, phase, batch, result, device
-                    )
-                print(epoch, format_result(phase, result, data_loader))
+    result = Munch()
+    for epoch in range(config.train.num_epochs):
+        for phase in phases:
+            result[phase] = Munch()
+            result[phase].total_loss = 0
+            result[phase].num_correct = 0
+            for batch in data_loader[phase]:
+                step(model, criterion, optimizer, phase, batch, result, device)
+        report_result(epoch, phases, result, data_loader)
 
 
 # def test(config):
