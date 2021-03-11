3a03e93cce645a7bbcab6b988f32272e76a60a6e
Jae Hee Lee <dschaehi@gmail.com>
2021-03-11 11:53:32 +0100

    Reduce cnn layer

--- qsr_learning/models.py
+++ qsr_learning/models.py
@@ -34,9 +34,9 @@ class VisionModule(nn.Module):
             nn.Conv2d(c, output_size, (h, w)),
             nn.BatchNorm2d(output_size),
             nn.ReLU(),
-            nn.Conv2d(output_size, output_size, 1),
-            nn.BatchNorm2d(output_size),
-            nn.ReLU(),
+            # nn.Conv2d(output_size, output_size, 1),
+            # nn.BatchNorm2d(output_size),
+            # nn.ReLU(),
         )
 
     def forward(self, image):
