a3b0abd94254664673af306ff6b093316082a6f5
Jae Hee Lee <dschaehi@gmail.com>
2021-03-23 22:12:20 +0100

    Update the model to mimic conv+lstm

--- qsr_learning/models.py
+++ qsr_learning/models.py
@@ -1,22 +1,67 @@
-from copy import deepcopy
 from typing import Tuple
 
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
-import torchvision
 from pytorch_lightning.metrics import Accuracy
 
 
-class VisionModule(nn.Module):
-    def __init__(
-        self,
-        vision_model: str,
-        image_size: Tuple[int, int, int],
-        output_size: int,
-        pretrained: bool = True,
-        freeze_image_encoder: bool = True,
+def build_stem(
+    feature_dim,
+    stem_dim,
+    module_dim,
+    num_layers=2,
+    with_batchnorm=True,
+    kernel_size=[3],
+    stride=[1],
+    padding=None,
+    subsample_layers=None,
+    acceptEvenKernel=False,
+):
+    """Taken from the FiLM repository"""
+    layers = []
+    prev_dim = feature_dim
+
+    if len(kernel_size) == 1:
+        kernel_size = num_layers * kernel_size
+    if len(stride) == 1:
+        stride = num_layers * stride
+    if padding is None:
+        padding = num_layers * [None]
+    if len(padding) == 1:
+        padding = num_layers * padding
+    if subsample_layers is None:
+        subsample_layers = []
+
+    for i, cur_kernel_size, cur_stride, cur_padding in zip(
+        range(num_layers), kernel_size, stride, padding
     ):
+        curr_out = module_dim if (i == (num_layers - 1)) else stem_dim
+        if cur_padding is None:  # Calculate default padding when None provided
+            if cur_kernel_size % 2 == 0 and not acceptEvenKernel:
+                raise (NotImplementedError)
+            cur_padding = cur_kernel_size // 2
+        layers.append(
+            nn.Conv2d(
+                prev_dim,
+                curr_out,
+                kernel_size=cur_kernel_size,
+                stride=cur_stride,
+                padding=cur_padding,
+                bias=not with_batchnorm,
+            )
+        )
+        if with_batchnorm:
+            layers.append(nn.BatchNorm2d(curr_out))
+        layers.append(nn.ReLU(inplace=True))
+        if i in subsample_layers:
+            layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+        prev_dim = curr_out
+    return nn.Sequential(*layers)
+
+
+class VisionModule(nn.Module):
+    def __init__(self, image_size: Tuple[int, int, int]):
         """Process the image.
 
         :param vision_model: vision model name
@@ -26,27 +71,21 @@ class VisionModule(nn.Module):
         super().__init__()
 
         # Image encoder
-        resnet = getattr(torchvision.models, vision_model)(pretrained=pretrained)
-        self.image_encoder = nn.Sequential(*deepcopy(list(resnet.children())[:-3]))
-        del resnet
-        # Freeze the image encoder weights
-        if freeze_image_encoder:
-            for param in self.image_encoder.parameters():
-                param.requires_grad = False
+        self.image_encoder = build_stem(
+            feature_dim=3,
+            stem_dim=64,
+            module_dim=64,
+            num_layers=6,
+            with_batchnorm=1,
+            subsample_layers=(1, 3, 5),
+        )
 
         self.image_size = image_size
         c, h, w = self.image_encoder_output_size
-        self.vision_module = nn.Sequential(
-            nn.Conv2d(c, output_size, (h, w)),
-            nn.BatchNorm2d(output_size),
-            nn.ReLU(),
-        )
 
     def forward(self, image):
-        batch_size = image.shape[0]
         image_features = self.image_encoder(image)
-        out = self.vision_module(image_features)
-        out = out.view(batch_size, -1)
+        out = image_features.view(image.shape[0], -1)
         return out
 
     @property
@@ -75,6 +114,7 @@ class LanguageModule(nn.Module):
         self,
         num_embeddings: int,
         embedding_dim: int,
+        hidden_size: int,
         question_len: int,
         # output_size: int,
     ):
@@ -84,7 +124,7 @@ class LanguageModule(nn.Module):
         self.question_len = question_len
         self.gru = nn.GRU(
             input_size=embedding_dim,
-            hidden_size=embedding_dim,
+            hidden_size=hidden_size,
             batch_first=True,
             bidirectional=False,
         )
@@ -113,13 +153,7 @@ class FusionModule(nn.Module):
     def __init__(self, image_feature_size, question_feature_size):
         super().__init__()
         self.fusion_module = nn.Sequential(
-            nn.Linear(
-                image_feature_size + question_feature_size,
-                image_feature_size + question_feature_size,
-            ),
-            nn.BatchNorm1d(image_feature_size + question_feature_size),
-            nn.ReLU(),
-            nn.Linear(image_feature_size + question_feature_size, 1),
+            nn.Linear(image_feature_size + question_feature_size, 1)
         )
 
     def forward(self, image_features: torch.Tensor, question_features: torch.Tensor):
@@ -136,26 +170,17 @@ class DRLNet(pl.LightningModule):
         image_size: Tuple[int, int, int],
         num_embeddings: int,
         embedding_dim: int,
+        hidden_size: int,
         question_len: int,
-        image_encoder_pretrained: bool = True,
-        freeze_image_encoder: bool = True,
         lr=0.001,
     ):
         super().__init__()
         self.save_hyperparameters()
 
+        self.vision_module = VisionModule(image_size)
         self.language_module = LanguageModule(
-            num_embeddings, embedding_dim, question_len
+            num_embeddings, embedding_dim, hidden_size, question_len
         )
-        output_size = self.language_module.output_size
-        self.vision_module = VisionModule(
-            vision_model,
-            image_size,
-            output_size,
-            image_encoder_pretrained,
-            freeze_image_encoder,
-        )
-        self.freeze_image_encoder = freeze_image_encoder
         self.fusion_module = FusionModule(
             self.vision_module.output_size, self.language_module.output_size
         )
@@ -171,8 +196,6 @@ class DRLNet(pl.LightningModule):
         return out
 
     def training_step(self, batch, batch_idx):
-        if self.freeze_image_encoder:
-            self.vision_module.image_encoder.eval()
         # Make prediction
         images, questions, answers = batch
         out = self(images, questions)
